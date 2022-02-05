7c4f87ef6e5c5ca462af736476041125a988d517
Sivert Havso <sih26@aber.ac.uk>
2022-02-05 00:54:35 +0000

    Use intra process communcation

--- gps_tools/src/utm_navsatfix_to_odometry.cpp
+++ gps_tools/src/utm_navsatfix_to_odometry.cpp
@@ -53,58 +53,56 @@ void UtmNavSatFixToOdometryComponent::navsatfix_callback(sensor_msgs::msg::NavSa
 
   LLtoUTM(fix->latitude, fix->longitude, northing, easting, zone);
 
-  if (odom_pub_) {
-    nav_msgs::msg::Odometry odom;
-    odom.header.stamp = fix->header.stamp;
+  nav_msgs::msg::Odometry::UniquePtr odom;
+  odom->header.stamp = fix->header.stamp;
 
-    if (frame_id_.empty()) {
-      if (append_zone_) {
-        odom.header.frame_id = fix->header.frame_id + "/utm_" + zone;
-      } else {
-        odom.header.frame_id = fix->header.frame_id;
-      }
+  if (frame_id_.empty()) {
+    if (append_zone_) {
+      odom->header.frame_id = fix->header.frame_id + "/utm_" + zone;
     } else {
-      if (append_zone_) {
-        odom.header.frame_id = frame_id_ + "/utm_" + zone;
-      } else {
-        odom.header.frame_id = frame_id_;
-      }
+      odom->header.frame_id = fix->header.frame_id;
     }
-
-    odom.child_frame_id = child_frame_id_;
-
-    odom.pose.pose.position.x = easting;
-    odom.pose.pose.position.y = northing;
-    odom.pose.pose.position.z = fix->altitude;
-
-    odom.pose.pose.orientation.x = 0;
-    odom.pose.pose.orientation.y = 0;
-    odom.pose.pose.orientation.z = 0;
-    odom.pose.pose.orientation.w = 1;
-
-    // Use ENU covariance to build XYZRPY covariance
-    std::array<double, 36> covariance = {{
-      fix->position_covariance[0],
-      fix->position_covariance[1],
-      fix->position_covariance[2],
-      0, 0, 0,
-      fix->position_covariance[3],
-      fix->position_covariance[4],
-      fix->position_covariance[5],
-      0, 0, 0,
-      fix->position_covariance[6],
-      fix->position_covariance[7],
-      fix->position_covariance[8],
-      0, 0, 0,
-      0, 0, 0, rot_cov_, 0, 0,
-      0, 0, 0, 0, rot_cov_, 0,
-      0, 0, 0, 0, 0, rot_cov_
-    }};
-
-    odom.pose.covariance = covariance;
-
-    odom_pub_->publish(odom);
+  } else {
+    if (append_zone_) {
+      odom->header.frame_id = frame_id_ + "/utm_" + zone;
+    } else {
+      odom->header.frame_id = frame_id_;
+    }
   }
+
+  odom->child_frame_id = child_frame_id_;
+
+  odom->pose.pose.position.x = easting;
+  odom->pose.pose.position.y = northing;
+  odom->pose.pose.position.z = fix->altitude;
+
+  odom->pose.pose.orientation.x = 0;
+  odom->pose.pose.orientation.y = 0;
+  odom->pose.pose.orientation.z = 0;
+  odom->pose.pose.orientation.w = 1;
+
+  // Use ENU covariance to build XYZRPY covariance
+  std::array<double, 36> covariance = {{
+    fix->position_covariance[0],
+    fix->position_covariance[1],
+    fix->position_covariance[2],
+    0, 0, 0,
+    fix->position_covariance[3],
+    fix->position_covariance[4],
+    fix->position_covariance[5],
+    0, 0, 0,
+    fix->position_covariance[6],
+    fix->position_covariance[7],
+    fix->position_covariance[8],
+    0, 0, 0,
+    0, 0, 0, rot_cov_, 0, 0,
+    0, 0, 0, 0, rot_cov_, 0,
+    0, 0, 0, 0, 0, rot_cov_
+  }};
+
+  odom->pose.covariance = covariance;
+
+  odom_pub_->publish(std::move(odom));
 }
 }  // namespace gps_tools
 
--- gps_tools/src/utm_odometry_to_navsatfix.cpp
+++ gps_tools/src/utm_odometry_to_navsatfix.cpp
@@ -42,7 +42,7 @@ void UtmOdometryToNavSatFixComponent::odom_callback(nav_msgs::msg::Odometry::Uni
 
   double northing, easting, latitude, longitude;
   std::string zone;
-  sensor_msgs::msg::NavSatFix fix;
+  sensor_msgs::msg::NavSatFix::UniquePtr fix;
 
   northing = odom->pose.pose.position.y;
   easting = odom->pose.pose.position.x;
@@ -50,7 +50,7 @@ void UtmOdometryToNavSatFixComponent::odom_callback(nav_msgs::msg::Odometry::Uni
   if (zone_.length() > 0) {
     // utm zone was supplied as a ROS parameter
     zone = zone_;
-    fix.header.frame_id = odom->header.frame_id;
+    fix->header.frame_id = odom->header.frame_id;
   } else {
     // look for the utm zone in the frame_id
     std::size_t pos = odom->header.frame_id.find("/utm_");
@@ -59,32 +59,32 @@ void UtmOdometryToNavSatFixComponent::odom_callback(nav_msgs::msg::Odometry::Uni
       return;
     }
     zone = odom->header.frame_id.substr(pos + 5, 3);
-    fix.header.frame_id = odom->header.frame_id.substr(0, pos);
+    fix->header.frame_id = odom->header.frame_id.substr(0, pos);
   }
 
   RCLCPP_INFO(this->get_logger(), "zone: %s", zone.c_str());
 
-  fix.header.stamp = odom->header.stamp;
+  fix->header.stamp = odom->header.stamp;
 
   UTMtoLL(northing, easting, zone, latitude, longitude);
 
-  fix.latitude = latitude;
-  fix.longitude = longitude;
-  fix.altitude = odom->pose.pose.position.z;
+  fix->latitude = latitude;
+  fix->longitude = longitude;
+  fix->altitude = odom->pose.pose.position.z;
 
-  fix.position_covariance[0] = odom->pose.covariance[0];
-  fix.position_covariance[1] = odom->pose.covariance[1];
-  fix.position_covariance[2] = odom->pose.covariance[2];
-  fix.position_covariance[3] = odom->pose.covariance[6];
-  fix.position_covariance[4] = odom->pose.covariance[7];
-  fix.position_covariance[5] = odom->pose.covariance[8];
-  fix.position_covariance[6] = odom->pose.covariance[12];
-  fix.position_covariance[7] = odom->pose.covariance[13];
-  fix.position_covariance[8] = odom->pose.covariance[14];
+  fix->position_covariance[0] = odom->pose.covariance[0];
+  fix->position_covariance[1] = odom->pose.covariance[1];
+  fix->position_covariance[2] = odom->pose.covariance[2];
+  fix->position_covariance[3] = odom->pose.covariance[6];
+  fix->position_covariance[4] = odom->pose.covariance[7];
+  fix->position_covariance[5] = odom->pose.covariance[8];
+  fix->position_covariance[6] = odom->pose.covariance[12];
+  fix->position_covariance[7] = odom->pose.covariance[13];
+  fix->position_covariance[8] = odom->pose.covariance[14];
 
-  fix.status.status = sensor_msgs::msg::NavSatStatus::STATUS_FIX;
+  fix->status.status = sensor_msgs::msg::NavSatStatus::STATUS_FIX;
 
-  fix_pub_->publish(fix);
+  fix_pub_->publish(std::move(fix));
 }
 }  // namespace gps_tools
 
