1485b412ddd8bc7eabdf57d80f64769327561dc7
Sivert Havso <sih26@aber.ac.uk>
2022-02-05 00:24:32 +0000

    Add UTM conversion node headers

---
+++ gps_tools/include/gps_tools/utm_odometry_to_navsatfix_component.hpp
@@ -0,0 +1,36 @@
+/*
+ * Translates nav_msgs/Odometry in UTM coordinates back into sensor_msgs/NavSat{Fix,Status}
+ * Useful for visualizing UTM data on a map or comparing with raw GPS data
+ * Added by Dheera Venkatraman (dheera@dheera.net)
+ */
+
+#ifndef GPS_TOOLS__UTM_ODOMETRY_TO_NAVSATFIX_COMPONENT_HPP_
+#define GPS_TOOLS__UTM_ODOMETRY_TO_NAVSATFIX_COMPONENT_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/nav_sat_fix.hpp>
+#include <sensor_msgs/msg/nav_sat_status.hpp>
+#include <gps_tools/conversions.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+
+#include <string>
+
+namespace gps_tools
+{
+class UtmOdometryToNavSatFixComponent : public rclcpp::Node
+{
+public:
+  explicit UtmOdometryToNavSatFixComponent(const rclcpp::NodeOptions & options);
+
+private:
+  void odom_callback(nav_msgs::msg::Odometry::UniquePtr odom);
+
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+  rclcpp::Publisher<sensor_msgs::msg::NavSatFix>::SharedPtr fix_pub_;
+
+  std::string frame_id_;
+  std::string zone_;
+};
+}  // namespace gps_tools
+
+#endif  // GPS_TOOLS__UTM_ODOMETRY_TO_NAVSATFIX_COMPONENT_HPP_
